5b14b106674575ebd24cf2c710d99570e2b33ee2
Arinze Owoh <arinzeowoh@gmail.com>
2025-02-05 10:04:58 +0100

    Add background images to projects and contact-me pages
    Redesign ClientLayout.tsx to animate a shadow glow on pathName change
    
    Add horizontal line to also glow on pathName change

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -2,14 +2,49 @@
 "use client";
 import { ThemeProvider } from "@/app/context/ThemeContext";
 import NavBar from "@/components/NavBar";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const ClientLayout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+  const [isFlashing, setIsFlashing] = useState(false);
+
+  useEffect(() => {
+    // Trigger flashing effect when the route changes
+    setIsFlashing(true);
+    const timer = setTimeout(() => setIsFlashing(false), 150); // Flash duration
+
+    return () => clearTimeout(timer);
+  }, [pathname]);
+
   return (
     <html lang="en">
       <ThemeProvider>
         <body className="antialiased bg-[url('/images/portfolio-background.webp')] bg-cover bg-center min-h-[100vh] p-[25px] flex justify-center items-center">
-          <div className="shadow-[0_0_30px_rgb(188,182,166)] relative">
-            <div className="absolute w-full">
+          {/* Flashing Horizontal Line */}
+          <div
+            className="absolute left-0 w-full h-[2px] top-[65vh]"
+            style={{
+              background: isFlashing
+                ? "rgb(255,255,200)" // Flash effect
+                : "transparent", // Normal color
+              boxShadow: isFlashing
+                ? "0 0 20px 2px rgb(255,255,200)" // Glow effect
+                : "0 0", // Default glow
+              transition:
+                "background 0.15s ease-in-out, box-shadow 0.15s ease-in-out",
+            }}
+          />
+          <div
+            className="relative shadow-[0_0_30px_rgb(188,182,166)]"
+            style={{
+              boxShadow: isFlashing
+                ? "0 0 30px 1px rgb(255,255,200)" // Flash effect
+                : "0 0 30px rgb(188,182,166)", // Normal shadow
+              transition: "box-shadow 0.15s ease-in-out",
+            }}
+          >
+            <div className="absolute w-full z-10">
               <NavBar />
             </div>
             {children}
--- src/app/contact-me/page.tsx
+++ src/app/contact-me/page.tsx
@@ -4,7 +4,7 @@ import { useThemeColor } from "@/app/context/ThemeContext";
 const ContactMe = () => {
   const themeColor = useThemeColor();
   return (
-    <div className="bg-[url('/images/home-page-background.png')] bg-cover bg-center min-h-full pt-[85px]">
+    <div className="bg-[url('/images/contact-me-page-background.webp')] bg-cover bg-center min-h-full pt-[85px]">
       <div
         className="w-[max(80vw,870px)] sm:h-[max(calc(35vw-85px),500px)] overflow-auto scrollbar-thin"
         style={{
