1baa676c404e0e8544292a09a2e8c298b96a699e
Arinze Owoh <arinzeowoh@gmail.com>
2025-03-16 11:36:44 +0100

    Chore: Edit placeholder text in projects page

--- src/app/contact-me/page.tsx
+++ src/app/contact-me/page.tsx
@@ -1,88 +1,20 @@
 "use client";
 import { useThemeColor } from "@/app/context/ThemeContext";
+import TranslucentCard from "@/components/TranslucentCard";
 
 const ContactMe = () => {
   const themeColor = useThemeColor();
   return (
     <div className="bg-[url('/images/contact-me-page-background.webp')] bg-cover bg-center min-h-full pt-[85px]">
       <div
-        className="w-[max(80vw,870px)] sm:h-[max(calc(35vw-85px),500px)] overflow-auto scrollbar-thin"
+        className="w-[max(80vw,870px)] sm:h-[max(calc(35vw-85px),500px)] overflow-auto scrollbar-thin flex items-center justify-center"
         style={{
           scrollbarColor: `${themeColor} #1F1F1F`, // Set thumb color and track color
         }}
       >
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
-        <p>Contact Me</p>
+        <TranslucentCard className="text-[80px] bg-contactMePrimary">
+          <p>This page is coming soon</p>
+        </TranslucentCard>
       </div>
     </div>
   );
